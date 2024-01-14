2483e3302746486637a5463608b08f0849668275
bbhitec <bboy.hitec@gmail.com>
2024-01-15 01:32:12 +0200

    basic strcmp refresh

--- strcmp.cpp
+++ strcmp.cpp
@@ -1,10 +1,9 @@
 //
 // [mst] strcmp.cpp strcmp doodle
-// description
+// simple string comparison run
 //
 // features, changelog:
 // -2021.10.11: -initial draft
-// -[wip]
 //
 
 
@@ -21,24 +20,12 @@ int strcmp_me (string str1, string str2){
         return -1;
     }
 
-    // [here]
-
-    /*
-    int i = 0;
-    while (str1.at(i) != '\n' && str2.at(i) != '\n') {
-        if (str1.at(i) != str2.at(i)) {
-            return -1;
-        }
-        std::cout << str1.at(i);
-        i++;
-    }
-    */
-
-    for ( std::string::iterator it=str1.begin(); it!=str1.end(); ++it) {
-        std::cout << *it;
+    // iterate the substring
+    for (auto i = 0; i< str2.size(); i++) {
+        if (str1[i] != str2[i]) return -1;
     }
 
-    return 0;
+    return 1;
 }
 
 ////////////////// DRIVER
@@ -49,7 +36,7 @@ int main()
     string teststr = "hello there";
     string substr = "hello";
 
-    std::cout << "comparing \'" << teststr << "\' and \'" << substr << "\' ..." << std::endl;
+    std::cout << "comparing \'" << teststr << "\' and \'" << substr << "\' ..." << std::endl << std::endl;
 
     std::cout << strcmp_me(teststr, substr) << std::endl;
 
