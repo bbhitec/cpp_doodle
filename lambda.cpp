135584d4980bb466b74739ed84f533a6085daeac
bbhitec <bboy.hitec@gmail.com>
2024-01-30 15:30:00 +0200

    feat: functional wrapper cpp

--- lambda.cpp
+++ lambda.cpp
@@ -4,12 +4,11 @@
     @brief  Some lambda, STL cpp functionality doodles
 
 	gains:
-    -basic lambda functions
     -cpp for_each
-	-[wip] combine in a best practices inclusive file?
+    -basic lambda functions
+	-using the function wrapper (C++11)
 
 
-    @author [mst]
     @version 0.1 2023.02
 */
 
@@ -17,6 +16,7 @@
 #include <iostream>         // usage of console prints
 #include <vector>
 #include <algorithm>		// usage of for_each
+#include <functional>
 
 using namespace std;
 
@@ -33,25 +33,45 @@ void print_vec(vector<int> nums) {
 }
 
 
+void func_print(int x) {cout << "your number is: " << x << endl;};
+void func_hi() {cout << "hello!" << endl;};
+
+
 ////////////////// DRIVER
 int main()
 {
 	cout << "[mst] lambda doodle" << endl << endl;
 
+
+	////////////// LAMBDA RELATED
+
 	// using iterators, lambda and for_each for a sexy code
     vector<int> nums = {-4,-1,0,3,10};
-
 	print_vec(nums);
 
 	// square all and sort (used in leetcode 977)
 	auto bgn = nums.begin();
     auto end = nums.end();
+
 	// [demo] we can use lambda expression with an explicit return type (not needed here as an a reference is used)
 	for_each(bgn,end,[](int &x) -> int {x*=x;});
     sort(bgn,end);
 
 	print_vec(nums);
 
+	////////////// FUNCTIONAL WRAPPER
+
+    // [demo] using functional wrapper for any signature-fitting callable object
+	function<void()> f = func_hi;
+	f();
+
+	function<void(int)> fp = func_print;
+	fp(48);
+
+	// or even a nameles lambda function
+	auto f_lambda = [](int x) {cout << "whoa, its: " << x << endl;};
+	function<void(int)> fl = f_lambda;
+	fl(32);
 
 	return 0;
 }
