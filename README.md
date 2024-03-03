c80789b4626c225ad11593cc3fe0496c568626c2
bbhitec <bboy.hitec@gmail.com>
2024-03-04 00:14:43 +0200

    adding functors usage

--- README.md
+++ README.md
@@ -3,19 +3,18 @@
 ## About
 Learning and best-practicing C++ language and its libraries 📝🧑‍🎓
 
-## WIP
-- [-] just imported. filter/clean apps
-- [ ] cpp/cs integration?
-- [ ] Make tools?
-- [ ] cpp: use of makefiles and dynamic/static includes
-<br><br>
-
 ## Gains
-- Best practices and familiarity towards Cpp
+- Best practices and familiarity with C++
+- Advanced C++ features: lambda, std::function, functors async
 
 ### Skills stack
-Cpp
-<br><br>
+![](https://shields.io/badge/-c++20-4377cb?logo=cplusplus)
+
+## WIP
+- [ ] bst: inOrderSuccessor
+- [ ] HTTP requests?
+- [ ] cpp: use of makefiles and dynamic/static includes
+- [ ] cpp/cs integration?
 
 ## Usage/How to run
 Most of the code uses native libraries<br>
--- lambda.cpp
+++ lambda.cpp
@@ -7,6 +7,7 @@
     -cpp for_each
     -basic lambda functions
 	-using the function wrapper (C++11)
+		more examples: https://youtu.be/DNp7fo226Qg?si=G3kAq1VpiYlVYGXH
 
 
     @version 0.1 2023.02
@@ -32,9 +33,17 @@ void print_vec(vector<int> nums) {
 	cout << endl;
 }
 
-
 void func_print(int x) {cout << "your number is: " << x << endl;};
 void func_hi() {cout << "hello!" << endl;};
+
+// [demo] this is a functor (aka function object): its an object that can be called like a function
+struct add_x {
+private:
+    int x;
+public:
+    add_x(int val) : x(val) {};  // the constructor will set the private inner member
+    int operator()(int y) const { return x + y; };
+};
 
 
 ////////////////// DRIVER
@@ -53,11 +62,24 @@ int main()
 	auto bgn = nums.begin();
     auto end = nums.end();
 
-	// [demo] we can use lambda expression with an explicit return type (not needed here as an a reference is used)
+	// [demo] we can use lambda expression with an explicit return type (not needed here as a reference is used)
 	for_each(bgn,end,[](int &x) -> int {x*=x;});
     sort(bgn,end);
 
 	print_vec(nums);
+
+
+	/////////////// FUNCTORS
+
+    // use callable object as a function
+    add_x add42(42); // create an instance of the functor class
+    int i = add42(8); // and "call" it
+    cout << "functor is: " << i << endl;
+
+    // use functors as arguments
+    vector<int> to_func ={0,1,2,3,4};
+    transform(to_func.begin(), to_func.end(), to_func.begin(), add_x(2));
+
 
 	////////////// FUNCTIONAL WRAPPER
 
